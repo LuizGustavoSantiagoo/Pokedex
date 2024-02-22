f30a996f38c8c1706de0ce55ae78b3c24cbebee6
LuizGustavoSantiago <128108006+LuizGustavoSantiagoo@users.noreply.github.com>
2024-02-21 21:15:45 -0300

    Add files via upload

---
+++ pokedex/assets/js/main.js
@@ -0,0 +1,35 @@
+const pokemonList = document.getElementById('pokemonList')
+const vejaMais = document.getElementById('vejaMais')
+const limit = 10;
+let offset = 0;
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => { 
+        const newHtml = pokemons.map((pokemon) =>  ` 
+            <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    
+                    <div class="detail">
+                    <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+
+                    <img src="${pokemon.foto}" 
+                    alt="${pokemon.name}">
+                </div>
+            </li> 
+    `).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItems(offset, limit);
+
+vejaMais.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItems(offset, limit)
+})
+
+
